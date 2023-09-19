abe2aa82f4bb1033d8fff95ce8a6a2befe3ec2ed
suhailbehlim <behlimsuhail@gmail.com>
2023-09-19 18:25:20 +0530

    scope

---
+++ script.js
@@ -0,0 +1,8 @@
+// quesion1. Var let const
+
+//  scope- scope is a certain reach an of program  where are defined variable is exist  and can be recognised.
+//   and beyond that it cannot be recognised ,called scope.
+//  type of scopes- 1.global scope 2.block scope 3.functional scope.
+
+console.log("hello");
+console.table(["apples", "oranges", "bananas"]);
